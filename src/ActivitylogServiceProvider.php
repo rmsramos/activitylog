951f48ef575f33c65abbfa751dff88ec6f4aa77f
Rômulo Ramos <rmsramos@gmail.com>
2023-09-06 01:39:51 -0300

    refactoring ActivitylogServiceProvider

--- src/ActivitylogServiceProvider.php
+++ src/ActivitylogServiceProvider.php
@@ -4,22 +4,15 @@ namespace Rmsramos\Activitylog;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Rmsramos\Activitylog\Commands\ActivitylogCommand;
 
 class ActivitylogServiceProvider extends PackageServiceProvider
 {
+    public static string $name = 'activitylog';
+
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('activitylog')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_activitylog_table')
-            ->hasCommand(ActivitylogCommand::class);
+            ->hasConfigFile('activitylog');
     }
 }

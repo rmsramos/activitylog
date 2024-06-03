ff3d1df7bb18c3e329f73cbea65cd104de54e9cd
Olivtradet <deoliveira.elias.eus@gmail.com>
2024-06-04 01:40:54 +0200

    added spanish traslations

---
+++ resources/lang/es/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'Tipo',
+        ],
+        'event' => [
+            'label' => 'Evento',
+        ],
+        'subject_type' => [
+            'label' => 'Asunto',
+        ],
+        'causer' => [
+            'label' => 'Usuario',
+        ],
+        'description' => [
+            'label' => 'Descripción',
+        ],
+        'properties' => [
+            'label' => 'Propiedades',
+        ],
+        'created_at' => [
+            'label' => 'Registrado en',
+        ],
+        'old' => [
+            'label' => 'Antiguo',
+        ],
+        'attributes' => [
+            'label' => 'Nuevo',
+        ],
+    ],
+];

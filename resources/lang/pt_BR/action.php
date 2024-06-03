fd85f8bd54d05e1ae67d8d07722e22bb49b90c0c
Rômulo Ramos <rmsramos@gmail.com>
2024-06-03 19:42:01 -0300

    add action lang

---
+++ resources/lang/pt_BR/action.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'modal' => [
+        'heading'     => 'Registro de Logs do usuário',
+        'description' => 'Acompanhe todas as atividades do usuário',
+        'tooltip'     => 'Atividades do usuário',
+    ],
+];

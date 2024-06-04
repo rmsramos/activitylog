9868d0706a3cb982933e68b4209ebd0b66e45b74
Olivtradet <deoliveira.elias.eus@gmail.com>
2024-06-04 09:11:47 +0200

    added new translations for Spanish

---
+++ resources/lang/es/action.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'modal' => [
+        'heading' => 'Registro de actividad del usuario',
+        'description' => 'Seguimiento de todas las actividades del usuario',
+        'tooltip' => 'Actividades del usuario',
+    ],
+];

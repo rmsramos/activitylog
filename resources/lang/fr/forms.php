65a4c3ef2d609d61e5e186e33ea01aea90c872ed
tbcy <43423272+tbcy@users.noreply.github.com>
2024-06-17 21:03:24 +0200

    Add french

---
+++ resources/lang/fr/forms.php
@@ -0,0 +1,31 @@
+<?php return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'Type',
+        ],
+        'event' => [
+            'label' => 'Événement',
+        ],
+        'subject_type' => [
+            'label' => 'Sujet',
+        ],
+        'causer' => [
+            'label' => 'Utilisateur',
+        ],
+        'description' => [
+            'label' => 'Description',
+        ],
+        'properties' => [
+            'label' => 'Propriétés',
+        ],
+        'created_at' => [
+            'label' => 'Enregistré à',
+        ],
+        'old' => [
+            'label' => 'Ancien',
+        ],
+        'attributes' => [
+            'label' => 'Nouveau',
+        ],
+    ],
+];

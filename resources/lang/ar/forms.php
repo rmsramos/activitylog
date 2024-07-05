c838067592f77362be755c7190de3439d75ee25d
Karam <karam.nassar@hotmail.com>
2024-07-05 12:30:11 +0300

    Add Arabic Translation

---
+++ resources/lang/ar/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name'     => [
+            'label' => 'النوع',
+        ],
+        'event'        => [
+            'label' => 'الحدث',
+        ],
+        'subject_type' => [
+            'label' => 'الموضوع',
+        ],
+        'causer'       => [
+            'label' => 'المستخدم',
+        ],
+        'description'  => [
+            'label' => 'الوصف',
+        ],
+        'properties'   => [
+            'label' => 'الخصائص',
+        ],
+        'created_at'   => [
+            'label' => 'تاريخ التسجيل',
+        ],
+        'old'          => [
+            'label' => 'القديم',
+        ],
+        'attributes'   => [
+            'label' => 'الجديد',
+        ],
+    ],
+];
---
+++ resources/lang/ar/tables.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'columns' => [
+        'log_name'     => [
+            'label' => 'النوع',
+        ],
+        'event'        => [
+            'label' => 'الحدث',
+        ],
+        'subject_type' => [
+            'label' => 'الموضوع',
+        ],
+        'causer'       => [
+            'label' => 'المستخدم',
+        ],
+        'properties'   => [
+            'label' => 'الخصائص',
+        ],
+        'created_at'   => [
+            'label' => 'تاريخ التسجيل',
+        ],
+    ],
+    'filters' => [
+        'created_at' => [
+            'label'         => 'تاريخ التسجيل',
+            'created_from'  => 'تاريخ الإنشاء من',
+            'created_until' => 'تاريخ الإنشاء حتى',
+        ],
+        'event'      => [
+            'label' => 'الحدث',
+        ],
+    ],
+];

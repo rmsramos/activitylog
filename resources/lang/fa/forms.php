58cd2f0d1adaf70fa491bf04aa85d1fa37fe27b1
Ali Salehi <alisalehi13800930@gmail.com>
2024-07-17 16:39:31 +0330

    add persian lang

---
+++ resources/lang/fa/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'نوع',
+        ],
+        'event' => [
+            'label' => 'رویداد',
+        ],
+        'subject_type' => [
+            'label' => 'مفعول',
+        ],
+        'causer' => [
+            'label' => 'کاربر',
+        ],
+        'description' => [
+            'label' => 'توضیحات',
+        ],
+        'properties' => [
+            'label' => 'تغییرات',
+        ],
+        'created_at' => [
+            'label' => 'ورود به سیستم در',
+        ],
+        'old' => [
+            'label' => 'قدیمی',
+        ],
+        'attributes' => [
+            'label' => 'جدید',
+        ],
+    ],
+];

91edf22115979fd8ddf881de5b80647e72065176
adereksisusanto <adereksisusanto@gmail.com>
2024-09-19 01:01:30 +0700

    Add lang id (indonesia)

---
+++ resources/lang/id/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'Jenis',
+        ],
+        'event' => [
+            'label' => 'Peristiwa',
+        ],
+        'subject_type' => [
+            'label' => 'Subjek',
+        ],
+        'causer' => [
+            'label' => 'Pengguna',
+        ],
+        'description' => [
+            'label' => 'Keterangan',
+        ],
+        'properties' => [
+            'label' => 'Properti',
+        ],
+        'created_at' => [
+            'label' => 'Masuk di',
+        ],
+        'old' => [
+            'label' => 'Tua',
+        ],
+        'attributes' => [
+            'label' => 'Baru',
+        ],
+    ],
+];

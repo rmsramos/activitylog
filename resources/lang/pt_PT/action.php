aa62ad86dc6765b106832704c9f32a380718f0a6
kid <kidiatoliny@akira-io.com>
2024-09-22 23:47:00 -0100

    feat: add Portuguese language support
    Extended the language support by adding Portuguese translations in the new pt_PT directory. Updated README to reflect the inclusion of Portuguese as a supported language.

---
+++ resources/lang/pt_PT/action.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'modal' => [
+        'heading'     => 'Registro de Logs do utilizador',
+        'description' => 'Acompanhe todas as atividades do utilizador.',
+        'tooltip'     => 'Atividades do utilizador',
+    ],
+];
---
+++ resources/lang/pt_PT/tables.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'columns' => [
+        'log_name' => [
+            'label' => 'Tipo',
+        ],
+        'event' => [
+            'label' => 'Evento',
+        ],
+        'subject_type' => [
+            'label' => 'Assunto',
+        ],
+        'causer' => [
+            'label' => 'Utilizador',
+        ],
+        'properties' => [
+            'label' => 'Propriedades',
+        ],
+        'created_at' => [
+            'label' => 'Criado em',
+        ],
+    ],
+    'filters' => [
+        'created_at' => [
+            'label'         => 'Criado em',
+            'created_from'  => 'Criado a partir de ',
+            'created_until' => 'Criado até ',
+        ],
+        'event' => [
+            'label' => 'Eventos',
+        ],
+    ],
+];

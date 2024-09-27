60f829857fabf6a45f87179a1cdf47524f77b5df
Marco Germani <info@marcogermani.it>
2024-09-27 20:33:35 +0200

    Added IT translations

---
+++ resources/lang/it/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'Tipo',
+        ],
+        'event' => [
+            'label' => 'Evento',
+        ],
+        'subject_type' => [
+            'label' => 'Soggetto',
+        ],
+        'causer' => [
+            'label' => 'Utente',
+        ],
+        'description' => [
+            'label' => 'Descrizione',
+        ],
+        'properties' => [
+            'label' => 'Proprietà',
+        ],
+        'created_at' => [
+            'label' => 'Loggato il',
+        ],
+        'old' => [
+            'label' => 'Vecchio',
+        ],
+        'attributes' => [
+            'label' => 'Nuovo',
+        ],
+    ],
+];

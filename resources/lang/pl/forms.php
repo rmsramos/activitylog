f4ca83d14973d053946a3bf74fad0be0976ced54
= <=>
2024-10-21 12:22:17 +0200

    PL translations added

---
+++ resources/lang/pl/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'Typ',
+        ],
+        'event' => [
+            'label' => 'Zdarzenie',
+        ],
+        'subject_type' => [
+            'label' => 'Element',
+        ],
+        'causer' => [
+            'label' => 'Użytkownik',
+        ],
+        'description' => [
+            'label' => 'Opis',
+        ],
+        'properties' => [
+            'label' => 'Właściwości',
+        ],
+        'created_at' => [
+            'label' => 'Data zdarzenia',
+        ],
+        'old' => [
+            'label' => 'Przed zmianą',
+        ],
+        'attributes' => [
+            'label' => 'Po zmianie',
+        ],
+    ],
+];

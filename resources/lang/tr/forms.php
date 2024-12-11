e0607685500c01301e0286d0f6cb8998c1d3db28
Mustafa Kerem CANSEV <mkeremcansev@gmail.com>
2024-12-11 14:58:32 +0300

    main: add TR language support

---
+++ resources/lang/tr/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'Tip',
+        ],
+        'event' => [
+            'label' => 'Olay',
+        ],
+        'subject_type' => [
+            'label' => 'Konu',
+        ],
+        'causer' => [
+            'label' => 'Kullanıcı',
+        ],
+        'description' => [
+            'label' => 'Açıklama',
+        ],
+        'properties' => [
+            'label' => 'Özellikler',
+        ],
+        'created_at' => [
+            'label' => 'Kayıt Tarihi',
+        ],
+        'old' => [
+            'label' => 'Eski',
+        ],
+        'attributes' => [
+            'label' => 'Yeni',
+        ],
+    ],
+];

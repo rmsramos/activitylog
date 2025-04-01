8ddbc6a0258e88811ab8d04bf511a3581e0287b6
Gydo Makkinga <3294611+makkinga@users.noreply.github.com>
2025-04-01 13:06:33 +0200

    Added Dutch translations

---
+++ resources/lang/nl/forms.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'fields' => [
+        'log_name' => [
+            'label' => 'Type',
+        ],
+        'event' => [
+            'label' => 'Event',
+        ],
+        'subject_type' => [
+            'label' => 'Onderwerp',
+        ],
+        'causer' => [
+            'label' => 'Gebruiker',
+        ],
+        'description' => [
+            'label' => 'Omschrijving',
+        ],
+        'properties' => [
+            'label' => 'Velden',
+        ],
+        'created_at' => [
+            'label' => 'Gelogd op',
+        ],
+        'old' => [
+            'label' => 'Oud',
+        ],
+        'attributes' => [
+            'label' => 'Nieuw',
+        ],
+    ],
+];

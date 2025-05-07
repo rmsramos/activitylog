30e94424c7581b7f5023b0a0f87f25d4abd1fec8
Asim M Al Twijry <AsimNet@users.noreply.github.com>
2025-05-07 23:49:35 +0300

    feat: Add Arabic translation for timeline.php
    This commit adds Arabic language support for the title and properties sections of the activity log, complementing the existing Arabic translations for columns and filters. This enhancement improves accessibility for Arabic-speaking users by providing a more complete localized experience.

--- resources/lang/ar/timeline.php
+++ resources/lang/ar/timeline.php
@@ -1,12 +1,12 @@
 <?php
 return [
     'title' => [
-        'modifiedTitle' => 'The <strong>%s</strong> was <strong>%s</strong> by <strong>%s</strong>. <br><small> Updated at: <strong>%s</strong></small>',
+        'modifiedTitle' => 'تم <strong>%s</strong> <strong>%s</strong> بواسطة <strong>%s</strong>. <br><small> تم التحديث في: <strong>%s</strong></small>',
     ],
     'properties' => [
-        'modifiedProperties'     => '%s %s the following: <br>%s',
+        'modifiedProperties'     => '%s %s التالي: <br>%s',
         'compareOldAndNewValues' => [
-            'notEquals' => '- %s from <strong>%s</strong> to <strong>%s</strong>',
+            'notEquals' => '- %s من <strong>%s</strong> إلى <strong>%s</strong>',
             'equals'    => '- %s <strong>%s</strong>',
         ],
         'getNewValues' => '- %s <strong>%s</strong>',

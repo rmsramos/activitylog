c517f8fb0f7b4b0cd1181f968e19323102fe0f60
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2026-07-31 19:35:16 -0300

    refactor: drop IconEntrySize alias, use Filament\Support\Enums\IconSize directly

--- resources/views/filament/infolists/components/time-line-icon-entry.blade.php
+++ resources/views/filament/infolists/components/time-line-icon-entry.blade.php
@@ -1,5 +1,5 @@
 @php
-    use Filament\Support\Enums\IconSize as IconEntrySize;
+    use Filament\Support\Enums\IconSize;
 @endphp
 
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
@@ -17,7 +17,7 @@
                 @if ($icon = $getIcon($state))
                     @php
                         $color = $getColor($state) ?? 'gray';
-                        $size = $getSize($state) ?? IconEntrySize::Large;
+                        $size = $getSize($state) ?? IconSize::Large;
                     @endphp
 
                     <x-filament::icon
@@ -25,12 +25,12 @@
                         @class([
                             'fi-in-icon-item',
                             match ($size) {
-                                IconEntrySize::ExtraSmall, 'xs' => 'fi-in-icon-item-size-xs h-3 w-3',
-                                IconEntrySize::Small, 'sm' => 'fi-in-icon-item-size-sm h-4 w-4',
-                                IconEntrySize::Medium, 'md' => 'fi-in-icon-item-size-md h-5 w-5',
-                                IconEntrySize::Large, 'lg' => 'fi-in-icon-item-size-lg h-6 w-6',
-                                IconEntrySize::ExtraLarge, 'xl' => 'fi-in-icon-item-size-xl h-7 w-7',
-                                IconEntrySize::TwoExtraLarge, IconEntrySize::ExtraExtraLarge, '2xl' => 'fi-in-icon-item-size-2xl h-8 w-8',
+                                IconSize::ExtraSmall, 'xs' => 'fi-in-icon-item-size-xs h-3 w-3',
+                                IconSize::Small, 'sm' => 'fi-in-icon-item-size-sm h-4 w-4',
+                                IconSize::Medium, 'md' => 'fi-in-icon-item-size-md h-5 w-5',
+                                IconSize::Large, 'lg' => 'fi-in-icon-item-size-lg h-6 w-6',
+                                IconSize::ExtraLarge, 'xl' => 'fi-in-icon-item-size-xl h-7 w-7',
+                                IconSize::TwoExtraLarge, '2xl' => 'fi-in-icon-item-size-2xl h-8 w-8',
                                 default => $size,
                             },
                             match ($color) {

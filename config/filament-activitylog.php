325f0bd5f347a997c7ec87fa24170fb640772415
jeffersongoncalves <jeffersongoncalves@users.noreply.github.com>
2026-07-31 22:47:50 +0000

    Fix styling

--- config/filament-activitylog.php
+++ config/filament-activitylog.php
@@ -1,23 +1,25 @@
 <?php
 
+use Rmsramos\Activitylog\Resources\Activitylog\ActivitylogResource;
+
 return [
     'resources' => [
-        'label' => 'Activity Log',
-        'plural_label' => 'Activity Logs',
-        'hide_restore_action' => false,
-        'restore_action_label' => 'Restore',
-        'hide_resource_action' => false,
+        'label'                     => 'Activity Log',
+        'plural_label'              => 'Activity Logs',
+        'hide_restore_action'       => false,
+        'restore_action_label'      => 'Restore',
+        'hide_resource_action'      => false,
         'hide_restore_model_action' => true,
-        'resource_action_label' => 'View',
-        'navigation_item' => true,
-        'navigation_group' => null,
-        'navigation_icon' => 'heroicon-o-shield-check',
-        'navigation_sort' => null,
-        'default_sort_column' => 'id',
-        'default_sort_direction' => 'desc',
-        'navigation_count_badge' => false,
-        'resource' => \Rmsramos\Activitylog\Resources\Activitylog\ActivitylogResource::class,
+        'resource_action_label'     => 'View',
+        'navigation_item'           => true,
+        'navigation_group'          => null,
+        'navigation_icon'           => 'heroicon-o-shield-check',
+        'navigation_sort'           => null,
+        'default_sort_column'       => 'id',
+        'default_sort_direction'    => 'desc',
+        'navigation_count_badge'    => false,
+        'resource'                  => ActivitylogResource::class,
     ],
-    'date_format' => 'd/m/Y',
+    'date_format'     => 'd/m/Y',
     'datetime_format' => 'd/m/Y H:i:s',
 ];
